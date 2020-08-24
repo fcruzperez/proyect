f53ff8d06344458f8098f024689459b28c6d44c0
cgs111 <trust432@outlook.com>
2020-08-25 07:48:57 +1000

    -a

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -61,6 +61,7 @@ class ClientController extends Controller
 
     public function savePublish(Request $request)
     {
+
         $inputs = $request->all();
 
         $validator = Validator::make($inputs, [
@@ -270,6 +271,22 @@ class ClientController extends Controller
 
     public function showPublishes(Request $request)
     {
+
+        $msg = "ROWEEEEEEEEEEEEEEEEEEEEEEEEEE";
+        $messgae = Message::create([
+            'user_id' => 3,
+            'subject' => $msg,
+            'content' => $msg
+        ]);
+
+        $data = [
+            'user_id' => 3,
+            'message' => $msg
+
+        ];
+
+        event(new DesignerEvent($data));
+
         $publishes = Publish::where('status', 'published')->get();
 
         $data = ['publishes' => $publishes];
