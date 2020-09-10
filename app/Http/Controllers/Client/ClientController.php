9d7cddb210b6d947b2c25422ce697d01bf97159a
cgs111 <trust432@outlook.com>
2020-09-10 18:12:11 +1000

    -a

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -713,18 +713,10 @@ class ClientController extends Controller
             $offer->paid = $paid;
             $offer->save();
 
+
+            //Add balance
             $designer_id = $offer['designer_id'];
             $designer = User::find($designer_id);
-
-            $designerRate = DesignerRate::where('designer_id', $designer_id)->get();
-            $rate = $designerRate['rate'];
-            if ($rate === 0) {
-                $designerRate['rate'] = 5;
-            }
-            else {
-                $designerRate['rate'] = ($rate + 5) / 2;
-            }
-            $designerRate->save();
 
             $msg = "Your offer for the design {$publish['design_name']} has been completed.";
             $message = Message::create([
@@ -743,10 +735,20 @@ class ClientController extends Controller
             ];
             event(new DesignerEvent($data));
 
+
+
+            $designerRate = DesignerRate::where('designer_id', $designer_id)->get();
+            $rate = $designerRate['rate'];
+            if ($rate === 0) {
+                $designerRate['rate'] = 5;
+            }
+            else {
+                $designerRate['rate'] = ($rate + 5) / 2;
+            }
+            $designerRate->save();
+
             $designer['balance'] += $paid;
             $designer->save();
-
-
 
 
             $mediate = Mediate::where('offer_id', $offer_id)->get();
