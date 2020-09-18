9d1b73bd66084c871f1621da56bc90b6d9601208
cgs111 <trust432@outlook.com>
2020-09-18 17:27:15 +1000

    -a

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -252,8 +252,8 @@
                                         @foreach($publish->deliveries as $key => $delivery)
 
                                         <div class="col-4">
-                                            <div class="col-4" style="margin-left: 15px;"><label>File{!! $key + 1 !!}:</label></div>
-                                            <div class="col-8" style="margin-left: 15px;">
+                                            <div class="col-4" style="margin-right: 15px;"><label>File{!! $key + 1 !!}:</label></div>
+                                            <div class="col-8" style="margin-right: 15px;">
                                                 <a class="btn btn-primary"
                                                    href="{{url('admin/delivery-download/'.$delivery->id)}}">
                                                     Download
