ffd5a30d57c725408458314e59322222f0d862f1
cgs111 <trust432@outlook.com>
2020-09-18 18:12:56 +1000

    -a

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -238,7 +238,7 @@
 
                                         @foreach($publish->deliveries as $key => $delivery)
 
-                                        <div class="col-4" style="margin-right: 3px;">
+                                        <div class="col-4" style="margin-left: 15px;">
                                             <div class="col-4"><label>File{!! $key + 1 !!}:</label></div>
                                             <div class="col-8">
                                                 <a class="btn btn-primary"
@@ -263,7 +263,7 @@
                                         @endforeach
 
                                         @if ($mediate['error_images'])
-                                        <div class="col-4" style="margin-left: 8px;">
+                                        <div class="col-4" style="margin-left: 15px;">
                                             <div class="col-4"><label>Errors:</label></div>
                                             <div class="col-8">
 
